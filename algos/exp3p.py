053f6a6df8491c4f04325e628c18bd0db60ea0fa
Lucas Willems <lcswillems@gmail.com>
2019-01-15 15:13:05 +0100

    Working exp3p

--- algos/exp3p.py
+++ algos/exp3p.py
@@ -3,26 +3,40 @@ import numpy as np
 class Exp3P:
     """Exp3.P algorithm."""
 
-    def __init__(self, nb_actions, η, γ, β):
-        self._nb_actions = nb_actions
+    def __init__(self, actions, η, γ, β, init_weights="uniform"):
+        self._actions = actions
+        self._nb_actions = len(self._actions)
         self.η = η
         self.γ = γ
         self.β = β
 
-        self.actions = np.arange(self.nb_actions)
-        self.w = 0.01 * np.ones(self.nb_actions)
-        self.w[np.random.randint(nb_actions)] = 1
+        if init_weights == "uniform":
+            self.w = self._uniform_weights()
+        elif init_weights == "hot":
+            self.w = self._hot_weights()
+        else:
+            raise ValueError("`{}` is not a correct value for `init_weights`.".format(init_weights))
 
-    @property
-    def nb_actions(self):
-        return self._nb_actions
+        self._update_policy()
+
+    def _uniform_weights(self):
+        return np.ones(self._nb_actions)
+
+    def _hot_weights(self):
+        w = 0.01 * np.ones(self._nb_actions)
+        w[np.random.randint(self._nb_actions)] = 1
+        return w
+
+    def _update_policy(self):
+        self._π = (1-self.γ) * self.w / np.sum(self.w) + self.γ / self._nb_actions
+        self.π = {self._actions[i]: self._π[i] for i in range(self._nb_actions)}
 
     def play(self):
-        self.σ = (1-self.γ) * self.w / np.sum(self.w) + self.γ / self.nb_actions
-        self.a = np.random.choice(self.actions, 1, p=self.σ)[0]
+        self.a = np.random.choice(self._actions, 1, p=self._π)[0]
         return self.a
 
     def analyze_feedback(self, u):
-        hot = np.array([int(self.a == i) for i in range(self.nb_actions)])
-        u2 = (u*hot + self.β)/self.σ[self.a]
+        hot = np.array([int(self.a == i) for i in range(self._nb_actions)])
+        u2 = (u*hot + self.β)/self._π
         self.w *= np.exp(self.η * u2)
+        self._update_policy()
